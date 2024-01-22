144f34c86edf419401ba44e85677846de97aefe1
Mitul <mitulgondaliya27@gmail.com>
2024-01-22 18:18:33 +0530

    login success

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import ValidateForm from 'src/app/helpers/validateforms';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon: string = 'fa fa-eye-slash';
+  signupForm!: FormGroup;
+
+  constructor(private fb : FormBuilder, private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      userName: ['', Validators.required],
+      passWord: ['', Validators.required],
+    })
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = 'fa-eye': this.eyeIcon = 'fa-eye-slash';
+    this.isText ? this.type = 'text': this.type = 'password';
+  }
+
+  onSignUp() {
+    if(this.signupForm.valid) {
+      console.log(this.signupForm.value);
+      this.authService.signUp(this.signupForm.value).subscribe({
+        next: res => {
+          alert(res.message);
+          this.signupForm.reset();
+          this.router.navigate(['login']);
+        },
+        error: err => {
+          alert(err?.error.meassage);
+        }
+      })
+    } else {
+      console.log('form is invalid');
+      ValidateForm.validateAllFormsFields(this.signupForm);
+    }
+  }
+  
+
+  
+
+
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = 'https://localhost:7129/'
+
+  constructor(private httpClient: HttpClient) { }
+
+  signUp(userObj: any) {
+    return this.httpClient.post<any>(this.baseUrl + "register", userObj);
+  }
+
+  login(loginObj: any) {
+    return this.httpClient.post<any>(this.baseUrl + "authenticate", loginObj)
+  }
+
+}
