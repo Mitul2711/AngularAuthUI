7ddce71a8da2f591cfb70209c77a1160eac29d4c
Mitul <mitulgondaliya27@gmail.com>
2024-01-23 12:01:59 +0530

    add username and email authentication

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -34,20 +34,17 @@ export class SignupComponent implements OnInit {
   }
 
   onSignUp() {
+    
     if(this.signupForm.valid) {
       console.log(this.signupForm.value);
-      this.authService.signUp(this.signupForm.value).subscribe({
-        next: res => {
-          alert(res.message);
-          this.signupForm.reset();
-          this.router.navigate(['login']);
-        },
-        error: err => {
-          alert(err?.error.meassage);
-        }
-      })
-    } else {
-      console.log('form is invalid');
+      this.authService.signUp(this.signupForm.value).subscribe(x=>{
+        alert(x.message)
+      },
+      err =>{
+        alert(err.error.message)
+      }
+      )
+    }else {
       ValidateForm.validateAllFormsFields(this.signupForm);
     }
   }
